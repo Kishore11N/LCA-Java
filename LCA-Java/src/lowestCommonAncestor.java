548c640abce3d57100915e6e6b8908284019120e
Kishore Muthu <muthuk@tcd.ie>
2020-10-28 16:46:21 +0000

    Syntax
    Some small syntax changed applied.

--- LCA-Java/src/lowestCommonAncestor.java
+++ LCA-Java/src/lowestCommonAncestor.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// A Binary Tree node 
 class Node {
+
     int data;
     Node left, right;
 
@@ -10,16 +10,17 @@ class Node {
         data = value;
         left = right = null;
     }
+
 }
 
-public class lowestCommonAncestor {
-
+public class lowestCommonAncestor{
 
     public static void main(String[] args) 
     { 
-
+      
     }
-    
+
+
     Node root;
     private List<Integer> path1 = new ArrayList<>();
     private List<Integer> path2 = new ArrayList<>();
@@ -27,50 +28,56 @@ public class lowestCommonAncestor {
     int findLCA(int n1, int n2) {
         path1.clear();
         path2.clear();
-        return findLCAInternal(root, n1, n2);
+        return recursiveLCA(root, n1, n2);
     }
 
-    private int findLCAInternal(Node root, int n1, int n2) {
+    int recursiveLCA(Node root, int n1, int n2) {
+        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) { 
+            System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing"); 
+            System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing"); 
+            return -1; 
+        } 
+  
+        int i; 
+        for (i = 0; i < path1.size() && i < path2.size(); i++) { 
+              
+        // System.out.println(path1.get(i) + " " + path2.get(i)); 
+            if (!path1.get(i).equals(path2.get(i))) 
+                break; 
+        } 
+  
+        return path1.get(i-1); 
+    } 
+    
 
-        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
-            System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing");
-            System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing");
-            return -1;
-        }
+     private boolean findPath(Node root, int n, List<Integer> path) {
 
-        int i;
-        for (i = 0; i < path1.size() && i < path2.size(); i++) {
-
-            if (!path1.get(i).equals(path2.get(i)))
-                break;
-        }
-
-        return path1.get(i - 1);
-    }
-
-    private boolean findPath(Node root, int n, List<Integer> path) {
-
+        
         if (root == null) {
             return false;
         }
 
         path.add(root.data);
 
-        if (root.data == n) {
+        if (root.data == n){
+            return true;
+        }
+        
+      
+        if(root.left != null && findPath(root.left, n, path)){
             return true;
         }
 
-        if (root.left != null && findPath(root.left, n, path)) {
+        if(root.right != null && findPath(root.right, n, path)){
             return true;
         }
 
-        if (root.right != null && findPath(root.right, n, path)) {
-            return true;
-        }
-
-        path.remove(path.size() - 1);
+        /* 
+        If the node with data n is not present in the subtree which has Node root as its root,
+        then remove the root's data from the path.
+        */
+        path.remove(path.size()-1); 
 
         return false;
     }
-
 }
