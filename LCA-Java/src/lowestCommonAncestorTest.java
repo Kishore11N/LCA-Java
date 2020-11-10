ea1d7dc0808b0d8fd2c5c69c40e5df23847a8ca0
Kishore Muthu <muthuk@tcd.ie>
2020-11-10 01:07:51 +0000

    Formatting

--- LCA-Java/src/lowestCommonAncestorTest.java
+++ LCA-Java/src/lowestCommonAncestorTest.java
@@ -11,13 +11,12 @@ public class lowestCommonAncestorTest {
 
     }
 
-
-    //Tests for LCA of Binary Tree
+    // Tests for LCA of Binary Tree
 
     @Test
 
     public void testFindLCA() {
-        //Creating a new binary tree
+        // Creating a new binary tree
         lowestCommonAncestor tree = new lowestCommonAncestor();
         tree.root = new Node(1);
         tree.root.left = new Node(2);
@@ -27,25 +26,21 @@ public class lowestCommonAncestorTest {
         tree.root.right.left = new Node(6);
         tree.root.right.right = new Node(7);
 
-        //Testing to see if correct outputs are produced
+        // Testing to see if correct outputs are produced
         assertEquals(2, tree.findLCA(4, 5));
         assertEquals(1, tree.findLCA(4, 6));
         assertEquals(1, tree.findLCA(3, 4));
         assertEquals(2, tree.findLCA(2, 4));
 
-        //Testing outputs when passing in invalid inputs
-        assertEquals(-1,tree.findLCA(7,8));
-        assertEquals(-1,tree.findLCA(-1,-2));
+        // Testing outputs when passing in invalid inputs
+        assertEquals(-1, tree.findLCA(7, 8));
+        assertEquals(-1, tree.findLCA(-1, -2));
 
-
-        //Creating a new binary tree with a null root
+        // Creating a new binary tree with a null root
         lowestCommonAncestor tree1 = new lowestCommonAncestor();
         tree1.root = null;
-        tree1.root.left = new Node(2);
-        tree1.root.right = new Node(3);
-        tree1.root.left.left = new Node(4);
-        
-        assertEquals(-1,tree1.findLCA(3,4));
+
+        assertEquals(-1, tree1.findLCA(3, 4));
 
     }
 
@@ -63,83 +58,80 @@ public class lowestCommonAncestorTest {
 
         List<Integer> path = new ArrayList<>();
 
-        //Testing for paths
+        // Testing for paths
         assertEquals(true, tree.findPath(tree.root, 1, path));
-        assertEquals(true,tree.findPath(tree.root.left,5,path));
-        assertEquals(true,tree.findPath(tree.root.right,7,path));    
-        
-        //Testing any error scenarios
-        assertEquals(false,tree.findPath(null,7,path));
-        assertEquals(false,tree.findPath(tree.root.right,8,path));
+        assertEquals(true, tree.findPath(tree.root.left, 5, path));
+        assertEquals(true, tree.findPath(tree.root.right, 7, path));
+
+        // Testing any error scenarios
+        assertEquals(false, tree.findPath(null, 7, path));
+        assertEquals(false, tree.findPath(tree.root.right, 8, path));
     }
 
-
-   
-    //Tests for LCA of a Directed Acyclic Graph
+    // Tests for LCA of a Directed Acyclic Graph
 
     @Test
 
-    public void testConstructor(){
-        //Creating a new Directed Acyclic Graph
+    public void testConstructor() {
+        // Creating a new Directed Acyclic Graph
         DAG Graph1 = new DAG(10);
-        Graph1.addEdge(1,2);
-        Graph1.addEdge(1,3);
-        Graph1.addEdge(2,4);
+        Graph1.addEdge(1, 2);
+        Graph1.addEdge(1, 3);
+        Graph1.addEdge(2, 4);
         Graph1.addEdge(4, 6);
         Graph1.addEdge(3, 5);
         Graph1.addEdge(5, 7);
         Graph1.addEdge(5, 8);
         Graph1.addEdge(5, 9);
 
-        //Testing getVertex
-        assertEquals(10,Graph1.getVertex());
+        // Testing getVertex
+        assertEquals(10, Graph1.getVertex());
 
-        //Testing getEdge
+        // Testing getEdge
         assertEquals(8, Graph1.getEdge());
 
-        //Testing for correct indegrees
-        assertEquals(0,Graph1.indegree(-1));
-        assertEquals(0,Graph1.indegree(1));
-        assertEquals(1,Graph1.indegree(5));
-        assertEquals(1,Graph1.indegree(7));
+        // Testing for correct indegrees
+        assertEquals(0, Graph1.indegree(-1));
+        assertEquals(0, Graph1.indegree(1));
+        assertEquals(1, Graph1.indegree(5));
+        assertEquals(1, Graph1.indegree(7));
 
-        //Testing for correct outdegrees
-        assertEquals(0,Graph1.outdegree(-1));
-        assertEquals(0,Graph1.outdegree(8));
-        assertEquals(1,Graph1.outdegree(2));
-        assertEquals(3,Graph1.outdegree(5));
+        // Testing for correct outdegrees
+        assertEquals(0, Graph1.outdegree(-1));
+        assertEquals(0, Graph1.outdegree(8));
+        assertEquals(1, Graph1.outdegree(2));
+        assertEquals(3, Graph1.outdegree(5));
 
-        //Testing for correct adjacents
-        assertEquals("[2, 3]",Graph1.adj(1).toString());
-        assertEquals("[5]",Graph1.adj(3).toString());
-        assertEquals("[7, 8, 9]",Graph1.adj(5).toString());
-
+        // Testing for correct adjacents
+        assertEquals("[2, 3]", Graph1.adj(1).toString());
+        assertEquals("[5]", Graph1.adj(3).toString());
+        assertEquals("[7, 8, 9]", Graph1.adj(5).toString());
 
     }
 
-
     @Test
 
-    public void testAddEdge(){
+    public void testAddEdge() {
         DAG Graph2 = new DAG(5);
         Graph2.addEdge(1, 2);
         Graph2.addEdge(2, -3);
         Graph2.addEdge(-3, -4);
-        
-        //Testing for edges between non-valid vertices
-        assertEquals(1,Graph2.getEdge());
-        
-        //Testing for edges between valid-vertices
+
+        // Testing for edges between non-valid vertices
+        assertEquals(1, Graph2.getEdge());
+
+        // Testing for edges between valid-vertices
         Graph2.addEdge(2, 3);
         Graph2.addEdge(3, 4);
-        assertEquals(3,Graph2.getEdge());
+        assertEquals(3, Graph2.getEdge());
 
     }
+
     @Test
 
-    public void testCyclic(){
-        
-        //Creating a directed cyclic graph
+    public void testCyclic() {
+
+        // Creating a directed cyclic graph
         DAG Graph3 = new DAG(5);
         Graph3.addEdge(1, 2);
         Graph3.addEdge(2, 3);
@@ -150,7 +142,7 @@ public class lowestCommonAncestorTest {
 
         assertEquals(true, Graph3.hasCycle());
 
-        //Creating a directed acyclic graph
+        // Creating a directed acyclic graph
         DAG Graph4 = new DAG(6);
         Graph4.addEdge(1, 2);
         Graph4.addEdge(2, 3);
@@ -165,9 +157,9 @@ public class lowestCommonAncestorTest {
 
     @Test
 
-    public void testLCA(){
-        //Creating a directed acyclic graph
-        DAG Graph5 = new DAG (20);
+    public void testLCA() {
+        // Creating a directed acyclic graph
+        DAG Graph5 = new DAG(20);
         Graph5.addEdge(1, 2);
         Graph5.addEdge(1, 3);
         Graph5.addEdge(2, 4);
@@ -181,45 +173,36 @@ public class lowestCommonAncestorTest {
         Graph5.addEdge(10, 11);
         Graph5.addEdge(11, 12);
 
-        //Testing LCA of 2 vertices within DAG
-        assertEquals(1,Graph5.findLCA(2, 3));
-        assertEquals(2,Graph5.findLCA(2, 2));
-        assertEquals(10,Graph5.findLCA(9, 10));
-        assertEquals(10,Graph5.findLCA(11, 13));
+        // Testing LCA of 2 vertices within DAG
+        assertEquals(1, Graph5.findLCA(2, 3));
+        assertEquals(2, Graph5.findLCA(2, 2));
+        assertEquals(10, Graph5.findLCA(9, 10));
+        assertEquals(10, Graph5.findLCA(11, 13));
 
-        //Testing LCA of invalid vertices
-        assertEquals(-1,Graph5.findLCA(0, 3));
-        assertEquals(-1,Graph5.findLCA(-1, -3));
+        // Testing LCA of invalid vertices
+        assertEquals(-1, Graph5.findLCA(0, 3));
+        assertEquals(-1, Graph5.findLCA(-1, -3));
 
-        //Creating a directed cyclic graph
-        DAG Graph6 = new DAG (20);
+        // Creating a directed cyclic graph
+        DAG Graph6 = new DAG(20);
         Graph6.addEdge(0, 1);
         Graph6.addEdge(0, 2);
         Graph6.addEdge(2, 3);
         Graph6.addEdge(3, 0);
         Graph6.addEdge(3, 4);
-    
 
-        //Testing to find LCA 
-        assertEquals(-1,Graph6.findLCA(0, 1));
-        assertEquals(-1,Graph6.findLCA(0, 2));
-        assertEquals(-1,Graph6.findLCA(2, 3));
-        assertEquals(-1,Graph6.findLCA(3, 4));
+        // Testing to find LCA
+        assertEquals(-1, Graph6.findLCA(0, 1));
+        assertEquals(-1, Graph6.findLCA(0, 2));
+        assertEquals(-1, Graph6.findLCA(2, 3));
+        assertEquals(-1, Graph6.findLCA(3, 4));
 
-
-        //Creating an empty graph
+        // Creating an empty graph
         DAG Graph7 = new DAG(20);
-        
-        //Testing for LCA in empty DAG
-        assertEquals(-1,Graph7.findLCA(1, 2));
 
-
-       
-
-
-
+        // Testing for LCA in empty DAG
+        assertEquals(-1, Graph7.findLCA(1, 2));
 
     }
-
 
 }
