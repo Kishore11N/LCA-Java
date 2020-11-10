389ff5eba4d2192ffd23d3153ed1e5e7648a55ff
Kishore Muthu <muthuk@tcd.ie>
2020-11-10 01:04:00 +0000

    DAG
    Added DAG implementation and test functions.

--- LCA-Java/src/lowestCommonAncestor.java
+++ LCA-Java/src/lowestCommonAncestor.java
@@ -1,5 +1,6 @@
-//DAG-Branch Code
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 class Node {
@@ -14,13 +15,11 @@ class Node {
 
 }
 
-public class lowestCommonAncestor{
+public class lowestCommonAncestor {
 
-    public static void main(String[] args) 
-    { 
-     System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
-
 
     Node root;
     private List<Integer> path1 = new ArrayList<>();
@@ -33,52 +32,235 @@ public class lowestCommonAncestor{
     }
 
     int recursiveLCA(Node root, int n1, int n2) {
-        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) { 
-            System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing"); 
-            System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing"); 
-            return -1; 
-        } 
-  
-        int i; 
-        for (i = 0; i < path1.size() && i < path2.size(); i++) { 
-              
-        // System.out.println(path1.get(i) + " " + path2.get(i)); 
-            if (!path1.get(i).equals(path2.get(i))) 
-                break; 
-        } 
-  
-        return path1.get(i-1); 
-    } 
-    
+        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
+            System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing");
+            System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing");
+            return -1;
+        }
 
-     boolean findPath(Node root, int n, List<Integer> path) {
+        int i;
+        for (i = 0; i < path1.size() && i < path2.size(); i++) {
 
-        
+            if (!path1.get(i).equals(path2.get(i)))
+                break;
+        }
+
+        return path1.get(i - 1);
+    }
+
+    boolean findPath(Node root, int n, List<Integer> path) {
+
         if (root == null) {
             return false;
         }
 
         path.add(root.data);
 
-        if (root.data == n){
-            return true;
-        }
-        
-      
-        if(root.left != null && findPath(root.left, n, path)){
+        if (root.data == n) {
             return true;
         }
 
-        if(root.right != null && findPath(root.right, n, path)){
+        if (root.left != null && findPath(root.left, n, path)) {
             return true;
         }
 
-        /* 
-        If the node with data n is not present in the subtree which has Node root as its root,
-        then remove the root's data from the path.
-        */
-        path.remove(path.size()-1); 
+        if (root.right != null && findPath(root.right, n, path)) {
+            return true;
+        }
+
+        /*
+         * If the node with data n is not present in the subtree which has Node root as
+         * its root, then remove the root's data from the path.
+         */
+        path.remove(path.size() - 1);
 
         return false;
+    }
+}
+
+
+
+
+
+
+// DAG Code from Branch
+
+class DAG {
+    private int V; // no. vertices
+    private int E; // no. edges
+    private ArrayList<Integer>[] adj; // adj[V] = adjacency list for vertex V
+    private int[] indegree; // indegree[V] = indegree of vertex V
+    private int[] outdegree;
+    private boolean marked[]; // list of visited vertices
+    private boolean hasCycle; // True if graph has cycle
+    private boolean stack[]; //
+
+    public DAG(int V) {
+        if (V < 0) {
+            throw new IllegalArgumentException("Number of vertices must be greater than 0");
+        }
+
+        this.V = V;
+        this.E = 0;
+        indegree = new int[V];
+        outdegree = new int[V];
+        marked = new boolean[V];
+        stack = new boolean[V];
+        adj = (ArrayList<Integer>[]) new ArrayList[V];
+
+        for (int v = 0; v < V; v++) {
+            adj[v] = new ArrayList<Integer>();
+        }
+    }
+
+    // Returns current vertex
+    public int getVertex() {
+        return V;
+    }
+
+    public int getEdge() {
+        return E;
+    }
+
+    // Adds directed edge from v to w
+    public void addEdge(int v, int w) {
+        if ((validateVertex(v) > 0) && (validateVertex(w) > 0)) {
+            adj[v].add(w);
+            indegree[w]++;
+            outdegree[v]++;
+            E++;
+        } else {
+            System.out.println("Please enter numbers between 0 and " + (V - 1));
+        }
+    }
+
+    private int validateVertex(int v) {
+        if (v < 0 || v >= V) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    // Returns number of directed edges to vertex v
+    public int indegree(int v) {
+        if (validateVertex(v) > 0) {
+            return indegree[v];
+        } else {
+            return 0;
+        }
+
+    }
+
+    // Returns number of directed edges from vertex v
+    public int outdegree(int v) {
+        if (validateVertex(v) > 0) {
+            return adj[v].size();
+        } else {
+            return 0;
+        }
+    }
+
+    // Returns the adjacent vertices to v
+    public ArrayList<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public boolean hasCycle() {
+        return hasCycle;
+    }
+
+    public void findCycle(int v) {
+        marked[v] = true;
+        stack[v] = true;
+
+        for (int w : adj(v)) {
+            if (!marked[w]) {
+                findCycle(w);
+            } else if (stack[w]) {
+                hasCycle = true;
+                return;
+            }
+        }
+        stack[v] = false;
+    }
+
+    // Method to implement lowest common ancestor
+    public int findLCA(int v, int w) {
+        findCycle(0);
+
+        if (hasCycle) // Graph is not DAG
+        {
+            return -1;
+        } else if (validateVertex(v) < 0 || validateVertex(w) < 0) {
+            // Not valid vertices, ie. non-negative
+            return -1;
+        } else if (E == 0) {
+            // Graph has no edges, ie. empty
+            return -1;
+        }
+
+        DAG reverse = reverse();
+
+        ArrayList<Integer> array1 = reverse.BFS(v);
+        ArrayList<Integer> array2 = reverse.BFS(w);
+        ArrayList<Integer> commonAncestors = new ArrayList<Integer>();
+
+        boolean found = false;
+
+        for (int i = 0; i < array1.size(); i++) {
+            for (int j = 0; j < array2.size(); j++) {
+                if (array1.get(i) == array2.get(j)) {
+                    commonAncestors.add(array1.get(i));
+                    found = true;
+                }
+            }
+        }
+
+        if (found) {
+            // Return first element in list - Lowest Common Ancestor
+            return commonAncestors.get(0);
+        } else {
+            return -1; // None found
+        }
+    }
+
+    // Prints BFS(Breadth-First search) from source s
+    public ArrayList<Integer> BFS(int s) {
+        ArrayList<Integer> order = new ArrayList<Integer>();
+        boolean visited[] = new boolean[V]; // Marks vertices as not visit
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            s = queue.poll(); // Sets s to the head of the list
+            order.add(s);
+
+            // Find adjacent vertices to s. If not visited,
+            // mark as visited (true) and enqueue
+            Iterator<Integer> i = adj[s].listIterator();
+
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+        return order;
+    }
+
+    // Reverse DAG
+    public DAG reverse() {
+        DAG reverse = new DAG(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj(v)) {
+                reverse.addEdge(w, v);
+            }
+        }
+        return reverse;
     }
 }
--- LCA-Java/src/lowestCommonAncestorTest.java
+++ LCA-Java/src/lowestCommonAncestorTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +11,13 @@ public class lowestCommonAncestorTest {
 
     }
 
+
+    //Tests for LCA of Binary Tree
+
     @Test
 
     public void testFindLCA() {
+        //Creating a new binary tree
         lowestCommonAncestor tree = new lowestCommonAncestor();
         tree.root = new Node(1);
         tree.root.left = new Node(2);
@@ -21,10 +27,25 @@ public class lowestCommonAncestorTest {
         tree.root.right.left = new Node(6);
         tree.root.right.right = new Node(7);
 
+        //Testing to see if correct outputs are produced
         assertEquals(2, tree.findLCA(4, 5));
         assertEquals(1, tree.findLCA(4, 6));
         assertEquals(1, tree.findLCA(3, 4));
         assertEquals(2, tree.findLCA(2, 4));
+
+        //Testing outputs when passing in invalid inputs
+        assertEquals(-1,tree.findLCA(7,8));
+        assertEquals(-1,tree.findLCA(-1,-2));
+
+
+        //Creating a new binary tree with a null root
+        lowestCommonAncestor tree1 = new lowestCommonAncestor();
+        tree1.root = null;
+        tree1.root.left = new Node(2);
+        tree1.root.right = new Node(3);
+        tree1.root.left.left = new Node(4);
+        
+        assertEquals(-1,tree1.findLCA(3,4));
 
     }
 
@@ -42,9 +63,163 @@ public class lowestCommonAncestorTest {
 
         List<Integer> path = new ArrayList<>();
 
+        //Testing for paths
         assertEquals(true, tree.findPath(tree.root, 1, path));
         assertEquals(true,tree.findPath(tree.root.left,5,path));
         assertEquals(true,tree.findPath(tree.root.right,7,path));    
+        
+        //Testing any error scenarios
+        assertEquals(false,tree.findPath(null,7,path));
         assertEquals(false,tree.findPath(tree.root.right,8,path));
     }
+
+
+   
+    //Tests for LCA of a Directed Acyclic Graph
+
+    @Test
+
+    public void testConstructor(){
+        //Creating a new Directed Acyclic Graph
+        DAG Graph1 = new DAG(10);
+        Graph1.addEdge(1,2);
+        Graph1.addEdge(1,3);
+        Graph1.addEdge(2,4);
+        Graph1.addEdge(4, 6);
+        Graph1.addEdge(3, 5);
+        Graph1.addEdge(5, 7);
+        Graph1.addEdge(5, 8);
+        Graph1.addEdge(5, 9);
+
+        //Testing getVertex
+        assertEquals(10,Graph1.getVertex());
+
+        //Testing getEdge
+        assertEquals(8, Graph1.getEdge());
+
+        //Testing for correct indegrees
+        assertEquals(0,Graph1.indegree(-1));
+        assertEquals(0,Graph1.indegree(1));
+        assertEquals(1,Graph1.indegree(5));
+        assertEquals(1,Graph1.indegree(7));
+
+        //Testing for correct outdegrees
+        assertEquals(0,Graph1.outdegree(-1));
+        assertEquals(0,Graph1.outdegree(8));
+        assertEquals(1,Graph1.outdegree(2));
+        assertEquals(3,Graph1.outdegree(5));
+
+        //Testing for correct adjacents
+        assertEquals("[2, 3]",Graph1.adj(1).toString());
+        assertEquals("[5]",Graph1.adj(3).toString());
+        assertEquals("[7, 8, 9]",Graph1.adj(5).toString());
+
+
+    }
+
+
+    @Test
+
+    public void testAddEdge(){
+        DAG Graph2 = new DAG(5);
+        Graph2.addEdge(1, 2);
+        Graph2.addEdge(2, -3);
+        Graph2.addEdge(-3, -4);
+        
+        //Testing for edges between non-valid vertices
+        assertEquals(1,Graph2.getEdge());
+        
+        //Testing for edges between valid-vertices
+        Graph2.addEdge(2, 3);
+        Graph2.addEdge(3, 4);
+        assertEquals(3,Graph2.getEdge());
+
+    }
+    @Test
+
+    public void testCyclic(){
+        
+        //Creating a directed cyclic graph
+        DAG Graph3 = new DAG(5);
+        Graph3.addEdge(1, 2);
+        Graph3.addEdge(2, 3);
+        Graph3.addEdge(3, 4);
+        Graph3.addEdge(4, 1);
+
+        Graph3.findCycle(1);
+
+        assertEquals(true, Graph3.hasCycle());
+
+        //Creating a directed acyclic graph
+        DAG Graph4 = new DAG(6);
+        Graph4.addEdge(1, 2);
+        Graph4.addEdge(2, 3);
+        Graph4.addEdge(3, 4);
+        Graph4.addEdge(4, 5);
+
+        Graph4.findCycle(1);
+
+        assertEquals(false, Graph4.hasCycle());
+
+    }
+
+    @Test
+
+    public void testLCA(){
+        //Creating a directed acyclic graph
+        DAG Graph5 = new DAG (20);
+        Graph5.addEdge(1, 2);
+        Graph5.addEdge(1, 3);
+        Graph5.addEdge(2, 4);
+        Graph5.addEdge(4, 6);
+        Graph5.addEdge(3, 5);
+        Graph5.addEdge(5, 7);
+        Graph5.addEdge(5, 8);
+        Graph5.addEdge(7, 10);
+        Graph5.addEdge(10, 9);
+        Graph5.addEdge(10, 13);
+        Graph5.addEdge(10, 11);
+        Graph5.addEdge(11, 12);
+
+        //Testing LCA of 2 vertices within DAG
+        assertEquals(1,Graph5.findLCA(2, 3));
+        assertEquals(2,Graph5.findLCA(2, 2));
+        assertEquals(10,Graph5.findLCA(9, 10));
+        assertEquals(10,Graph5.findLCA(11, 13));
+
+        //Testing LCA of invalid vertices
+        assertEquals(-1,Graph5.findLCA(0, 3));
+        assertEquals(-1,Graph5.findLCA(-1, -3));
+
+        //Creating a directed cyclic graph
+        DAG Graph6 = new DAG (20);
+        Graph6.addEdge(0, 1);
+        Graph6.addEdge(0, 2);
+        Graph6.addEdge(2, 3);
+        Graph6.addEdge(3, 0);
+        Graph6.addEdge(3, 4);
+    
+
+        //Testing to find LCA 
+        assertEquals(-1,Graph6.findLCA(0, 1));
+        assertEquals(-1,Graph6.findLCA(0, 2));
+        assertEquals(-1,Graph6.findLCA(2, 3));
+        assertEquals(-1,Graph6.findLCA(3, 4));
+
+
+        //Creating an empty graph
+        DAG Graph7 = new DAG(20);
+        
+        //Testing for LCA in empty DAG
+        assertEquals(-1,Graph7.findLCA(1, 2));
+
+
+       
+
+
+
+
+    }
+
+
 }
