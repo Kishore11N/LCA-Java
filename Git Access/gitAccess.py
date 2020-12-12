bd1501366400afdeae700c46a3f00280c13f02ee
Kishore Muthu <muthuk@tcd.ie>
2020-12-12 01:03:04 +0000

    Validate Function
    I have added a function called validate() that will ensure only valid access tokens, usernames and repositories are accepted by the file. Currently output is the names of all the repositories belonging to a given user on GitHub

--- Git Access/gitAccess.py
+++ Git Access/gitAccess.py
@@ -1,13 +1,56 @@
 from github import Github
 from pprint import pprint
 
-username = input("Please enter username : ")
-access_token = input("Please enter your access token : ")
-repoName = input("Please enter the name of the repository : ")
+def validate():
 
-g = Github(access_token)
-user = g.get_user(username)
-for repo in user.get_repos():
-    print("The name of the repositories are : " + repo.name)
+    validDetails = False
+    
+    while(validDetails == False):
 
+        validToken = False
+        validUser = False
+        validRepo = False
+    
+        while (validToken == False):         
+            try:      
+                access_token = input("Please enter your access token : ")  
+                g = Github(access_token)
+                g.get_user("esjmb")
+                validToken = True
+            except:
+                print("Access token is invalid, please try again")
 
+        while (validUser == False):          
+            try:
+                user_github = input("Please enter user you would like to view : ")
+                user = g.get_user(user_github)
+                validUser = True             
+            except:
+                print("User does not exist, please try again")
+
+        while (validRepo == False):           
+            try:     
+               repoName = input("Please enter the name of the repository : ") 
+               repo = user.get_repo(repoName)   
+               validRepo = True               
+            except:
+                print("Repository name is invalid, please try again")
+
+        if validUser == True & validRepo == True :
+            if validToken ==True:
+                validDetails = True
+
+    return [validDetails,access_token,user_github,repoName]
+
+valid = validate()
+isValid = valid[0]
+access_token = valid[1]
+user_github = valid[2]
+repoName = valid[3]
+
+if isValid == True:
+    g = Github(access_token)
+    user = g.get_user(user_github)
+    for repo in user.get_repos():
+        print(repo.name)
+    
