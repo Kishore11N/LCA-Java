63c6f61d60272963b3396b97faeb93f6ecbe879a
Kishore Muthu <muthuk@tcd.ie>
2020-12-15 15:32:27 +0000

    New functions
    Removed the getInformation() function, but getAllRepos() function was added. This collects relevant information required and returns it as an array and the information collected is outputted in JSON format.

--- Git Access/gitAccess.py
+++ Git Access/gitAccess.py
@@ -1,5 +1,6 @@
 from github import Github
 from pprint import pprint
+import json
 
 def validate():
 
@@ -43,19 +44,56 @@ def validate():
     return [validDetails,access_token,user_github,repoName]
 
 
-def getInformation():
+def getAllRepos(repo):
+  numOfRepos = []
+  contributor_names = []
+  no0fContributions = []
+  personCounter = 0
+  repoCounter = 0
+  contributors = repo.get_contributors()
+  
+  for contributor in contributors:
+    personCounter += 1
+    print("Countributor number : " + str(personCounter))
+    contributorLogin = str(contributor.login)
+    contributionTime = str(contributor.contributions)
+    user1 = g.get_user(contributorLogin)
+    
+    for repo in user1.get_repos():
+          repoCounter += 1
+    
+    contributor_names .append(contributorLogin)
+    no0fContributions.append(contributionTime)
+    numOfRepos.append(str(repoCounter))
+    
+  #returns a list containing three lists regarding information about committers of a repo
+  return [contributor_names,no0fContributions,numOfRepos]
 
-    valid = validate()
-    isValid = valid[0]
-    access_token = valid[1]
-    user_github = valid[2]
-    repoName = valid[3]
 
-    if isValid == True:
-        g = Github(access_token)
-        user = g.get_user(user_github)
-        for repo in user.get_repos():
-            print(repo.name)
-        
+valid = validate()
+isValid = valid[0]
+access_token = valid[1]
+user_github = valid[2]
+repoName = valid[3]
 
-getInformation()
+
+if isValid == True:
+    g = Github(access_token)
+    user = g.get_user(user_github)
+    for repo in user.get_repos():
+        if(repo.name == repoName):
+           info = getAllRepos(repo)
+           contributors = info[0]
+           commits = info[1]
+           experience = info[2]       
+           data = []
+
+           for cr, cn, rn in zip(contributors, commits, experience):
+            personInfo = {'person': {'login': cr, 'contributions': cn, 'repoNum': rn}}
+            data.append(personInfo)
+
+            y = json.dumps(data)
+            pprint(y)
+            
+         
+
